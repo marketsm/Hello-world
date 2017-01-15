1805db851c23a95f1b22c2639578805796c77745
Christoph Marketsmüller <c.marketsmueller@gmail.com>
2017-01-15 21:23:21 +0100

    add existing Files

---
+++ src/main/java/org/jboss/tools/examples/first_maven_projekt/App.java
@@ -0,0 +1,13 @@
+package org.jboss.tools.examples.first_maven_projekt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
